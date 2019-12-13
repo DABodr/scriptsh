87b45987b014cbd28094b70aae8f867787a634ea
RSAP <46652797+DABodr@users.noreply.github.com>
2019-12-13 11:46:19 +0100

    Add files via upload

---
+++ play_eti_zmq/eti2zmq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ /home/$USER/dab/eti-tools/eti2zmq -i /home/$USER/Bureau/eti_vincent/SWR-RP_12122019_093906.eti -l -a -d -o "zmq+tcp://*:18081"
+
+#DR-Saarland_12122019_092914.eti
+#DR-Deutschland_12122019_092118.eti
+#LIEGE-1_12122019_092744.eti
+#LIEGE-2_12122019_092330.eti
+#NAM-LUX-1_12122019_091553.eti
+#NAM-LUX-2_12122019_091722.eti
+#SWR-RP_12122019_093906.eti
+#0,0x0425.bin.eti
